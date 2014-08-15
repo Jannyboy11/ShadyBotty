f52f4ea3ab73734ceddb72aea36bdff7bd44fb54
Jannyboy11 <janstoomlocomotief@hotmail.com>
2014-08-16 01:27:32 +0200

    test

--- src/main/ShadyBotty.java
+++ src/main/ShadyBotty.java
@@ -3,9 +3,17 @@ package main;
 import org.jibble.pircbot.PircBot;
 
 public class ShadyBotty extends PircBot {
-	
+
 	public ShadyBotty(){
 		this.setName("ShadyBotty");
+	}
+
+	public void onMessage(String channel, String sender,
+			String login, String hostname, String message) {
+		if (message.equalsIgnoreCase("time")) {
+			String time = new java.util.Date().toString();
+			sendMessage(channel, sender + ": The time is now " + time);
+		}
 	}
 
 }
