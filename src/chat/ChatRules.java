cf425bafce24599d02306febb64902d135e85a4f
Jannyboy11 <janstoomlocomotief@hotmail.com>
2014-08-16 14:18:24 +0200

    privilages

---
+++ src/chat/ChatRules.java
@@ -0,0 +1,45 @@
+package chat;
+
+public class ChatRules {
+	
+	
+	
+	
+	public ChatRules(){
+		
+	}
+	
+	//returs the amount of seconds the user should be timed out. If the result is -1, then no timeout should be performed.
+	public int checkMessage(String nick, String message){
+		int result = 0;
+		if ((new Privileges(nick)).links() == -1 || checkLink(message) == -1){
+			return -1;
+		} 
+		
+		if (checkCaps(message) != -1){
+			return checkCaps(s);
+		} else if (checkEmoticons(s) != -1) {
+			return checkEmoticons(s);
+		}
+		
+		
+		
+		
+	}
+	
+	private int checkEmoticons(String s) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	private int checkLink(String s) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int checkCaps(String s){
+		return 0;
+	}
+	
+	
+}
---
+++ src/chat/Privileges.java
@@ -0,0 +1,34 @@
+package chat;
+
+public class Privileges {
+	
+	private String nick;
+	
+	private int links;
+	private int filters; // used for caps & emoticons. 
+	private boolean premium;
+	
+	
+	public Privileges(String n){
+		nick = n;
+		//TODO get al the user privilges from the ini file and set the fields.
+		//filters: read from ini file how many times the user spammed to much caps or too much emoticons. if the user has bought filters, set it to -1;
+		//links: read from ini file how many times the users typed a link. if the user has bought links, set it to -1;
+		//premium: read from inin file whether the user has premium.
+		
+		
+	}
+	
+	public int links(){
+		return links;
+	}
+	
+	public int filters(){
+		return filters;		
+	}
+	
+	public boolean hasPremium(){
+		return premium;
+	}	
+
+}
