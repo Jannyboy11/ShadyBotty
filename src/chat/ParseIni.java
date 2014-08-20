e1b7c51cd6d0f2a2a62fc3041d67c88682249db4
Jannyboy11 <janstoomlocomotief@hotmail.com>
2014-08-20 16:30:43 +0200

    parseini

--- src/chat/ParseIni.java
+++ src/chat/ParseIni.java
@@ -2,10 +2,19 @@ package chat;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
 
 import org.ini4j.Wini;
 
 public class ParseIni {
+	
+	File theIniFile;
+	
+	public ParseIni(File file){
+		
+	}
 
 	public static void main(String[] args){
 		
@@ -22,6 +31,38 @@ public class ParseIni {
 			e.printStackTrace();
 		}
 
+	}
+	
+	public List<String> getSettings(String nick){
+		
+		Wini ini;
+		List<String> result = new LinkedList<String>();
+		try {
+			ini = new Wini(theIniFile);
+			Scanner scanner = new Scanner(theIniFile);
+			
+			String foundprofile = scanner.findWithinHorizon("[" + nick + "]", ini.size());
+			while (scanner.hasNextLine()){
+				String next = scanner.next();
+				if (!(next.startsWith("["))){
+					result.add(next);
+				} else {
+					return result;
+				}
+				
+			}
+			
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		//there was no profile for this user
+		return null;
 	}
 
 }
