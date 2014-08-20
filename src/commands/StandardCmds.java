03efda496bf395650e25f36cdb1373d9bf99769d
Jannyboy11 <janstoomlocomotief@hotmail.com>
2014-08-20 17:17:42 +0200

    pointscommand made

--- src/commands/StandardCmds.java
+++ src/commands/StandardCmds.java
@@ -1,5 +1,61 @@
 package commands;
 
+import java.util.HashMap;
+
+import points.Chips;
+import points.Points;
+import main.ShadyBotty;
+
 public class StandardCmds {
+
+	private static ShadyBotty botty;
+	private static long latestPointsRequest;
+	private static HashMap<String, Long> latestRequestByUser;
+
+	public StandardCmds(ShadyBotty bot){
+		botty = bot;
+		setLatestPointsRequest(System.currentTimeMillis());
+	}
+
+	public void setLatestPointsRequest(long value){
+		latestPointsRequest = value;
+	}
+
+	public static boolean canRequestPoints(String nick){
+		if (ShadyBotty.database.getPrivileges(nick).getCooldown() == -1){
+			return true;
+		}
+		long call =  System.currentTimeMillis();
+		if (latestRequestByUser.containsKey(nick)){
+			Long latestrequest = latestRequestByUser.get(nick);
+			if (call - latestrequest > 60000){
+				latestRequestByUser.put(nick, call);
+				return true;
+			}
+		} else if (call - latestPointsRequest > 5000){
+			latestRequestByUser.put(nick, call);
+			return true;			
+		}
+		return false;
+	}
+
+	public static boolean requestPoints(String nick){		
+		if (canRequestPoints(nick)){
+			String toSend = nick + " has " + Points.getPoints(nick);
+			if (Chips.getChips(nick) != 0)
+				toSend += " and " + Chips.getChips(nick);
+			if (ShadyBotty.database.getPrivileges(nick).getFaction().equalsIgnoreCase("jb940"))
+				toSend += ". " + nick + " also has 1 GodPoint! Kappa/";
+				
+			
+			
+			botty.sendToBunny(toSend);
+			return true;
+		}
+		return false;
+	}
+
+
+
 
 }
--- src/points/Chips.java
+++ src/points/Chips.java
@@ -2,4 +2,9 @@ package points;
 
 public class Chips {
 
+	public static int getChips(String nick) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
