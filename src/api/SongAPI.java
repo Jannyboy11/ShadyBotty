0495f6716aefa908cd5b5d7e525bf083b46bd5b7
JB940 <Jordybr94@gmail.com>
2014-08-29 18:41:10 +0200

    omething for song

---
+++ src/api/SongAPI.java
@@ -0,0 +1,54 @@
+package api;
+
+import java.io.IOException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class SongAPI {
+	
+	private static final String API = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=shadybunnylive&api_key=6bb626000cf39dabadb9e904e613d024&format=json&limit=1";
+///,\"recenttracks\",\"track\",\"name\")
+	private static String latestMusic = "none";
+	public SongAPI() {
+	}
+	
+	public static JSONObject getJSONSong() {
+		try {
+			return Parser.readJsonFromUrl(API);
+		} catch (JSONException | IOException e) {
+		}
+		return null;
+	}
+	
+	public static String getSongNameShady() {
+		JSONObject json = getJSONSong();
+
+		if (json != null)
+			return json.optJSONObject("recenttracks").optJSONObject("track").optString("name");
+		return "none";
+	}
+	
+	public static String getArtistNameShady() {
+		JSONObject json = getJSONSong();
+
+		if (json != null)
+			return json.optJSONObject("recenttracks").optJSONObject("track").optJSONObject("artist").optString("#text");
+		return "none";
+	}
+	public static void checkMusic() {
+		String musicAPI = getArtistNameShady() + " - " + getSongNameShady();
+		if (musicAPI == "none" || musicAPI == null || musicAPI == latestMusic)
+			return;
+		latestMusic = musicAPI;
+	}
+	
+	public static String getLatestmusic() {
+		return latestMusic;
+	}
+	
+	public static void setLatestmusic(String music) {
+		return;
+	}
+	
+}
