0d1e50e88f0c482d9976e5662cf2330375370709
JB940 <Jordybr94@gmail.com>
2014-09-05 15:20:45 +0200

    za

--- src/main/ShadyBottyMain.java
+++ src/main/ShadyBottyMain.java
@@ -13,18 +13,22 @@ public class ShadyBottyMain {
 		// Now start our bot up.
 		ShadyBotty bot = new ShadyBotty();
 		SubBotty subbot = new SubBotty();
+		PrivateBotty privbot = new PrivateBotty();
 		// Enable debugging output.
-		bot.setVerbose(true);
+		bot.setVerbose(false);
 		subbot.setVerbose(false);
+		privbot.setVerbose(true);
 		// Connect to the IRC server.
 		BufferedReader blub = new BufferedReader(new FileReader("C:/wachtwoord.txt"));
 		String password = blub.readLine();
 		blub.close();
 		subbot.connect("irc.twitch.tv",6667,password);
 		bot.connect("irc.twitch.tv",6667,password);
+		privbot.connect("199.9.248.232",80,password);
 		bot.joinChannel("#shadybunny");
 		// Join the #pircbot channel.
 		subbot.joinChannel("#shadybunny");
+		privbot.joinChannel("#_shadybottyroom");
 	}
 
 }
