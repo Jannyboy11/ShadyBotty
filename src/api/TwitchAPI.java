4cd3c2f733c145de0b326520fff954a58350a40b
JB940 <Jordybr94@gmail.com>
2015-01-31 19:08:15 +0100

    Woop

--- src/api/TwitchAPI.java
+++ src/api/TwitchAPI.java
@@ -1,6 +1,10 @@
 package api;
 
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -9,9 +13,11 @@ import org.json.JSONObject;
 public class TwitchAPI {
 	private static final String API = "https://api.twitch.tv/kraken/";
 	private static final String SHADY = "streams/shadybunny";
-	
+	private static final String SHADYCHANNEL = "channels/shadybunny";
+	private static final String SECRETTITLE = "kme7og92u2qluzu8f6ax2mo4fh4lja";
+
 	public TwitchAPI() {
-		
+
 	}
 
 	public static JSONObject getJSONShady() {
@@ -21,20 +27,53 @@ public class TwitchAPI {
 		}
 		return null;
 	}
-	
+
 	public static JSONObject getJSONStreamShady() {
 		JSONObject json = getJSONShady();
 		if (json != null)
 			return json.optJSONObject("stream");
 		return null;
 	}
-	
+
 	public static int getViewersShady() {
 		JSONObject json = getJSONStreamShady();
 		if (json != null)
 			return json.optInt("viewers");
 		return 0;
 	}
+
+	public static void PUT(String url, String[] names,String[] values) throws Exception {
+		String toWrite = "?"+names[0] + "=" + values[0];
+		for (int i = 1; i < names.length; i++) {
+			toWrite += "&"+names[i]+"="+values[i];
+		}
+		URL site = new URL(url + toWrite);
+		HttpURLConnection httpCon = (HttpURLConnection) site.openConnection();
+		httpCon.setDoOutput(true);
+		httpCon.setRequestMethod("PUT");
+ 	httpCon.addRequestProperty("Authorization", "OAuth 493i5sfeqdpe89v6y6xwa0ltdtlyp4");
+		OutputStreamWriter out = new OutputStreamWriter(
+		    httpCon.getOutputStream());
+
+
+		out.write(toWrite);
+		System.out.println(toWrite);
+		out.close();
+		httpCon.getInputStream();
+	}
 	
+	public static void updateChannel(String theTitle) {
+		try {
+			PUT(API + SHADYCHANNEL,new String[] {"oauth_token","channel[status]"}, new String[] {SECRETTITLE,theTitle});
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		updateChannel("Gods of the arena Kappa happy CodeDay JB!".replace(" ", "+"));
+	}
+
 
 }
---
+++ src/trivia/TriviaThread.java
@@ -0,0 +1,192 @@
+package trivia;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Random;
+
+import chat.Nicknames;
+import points.Points;
+import main.ShadyBotty;
+import main.ShadyBottyMain;
+
+public class TriviaThread extends Thread{
+	public static final int QUESTIONAMOUNT = 173;
+	public static ArrayList<String> questions = new ArrayList<String>(QUESTIONAMOUNT);
+	public static String topic = "";
+	public static String question ="";
+	public static String hint ="";
+	public static ArrayList<String> answers = new ArrayList<String>();
+	public static String displayAnswer = "";
+	public static boolean off;
+	public static LinkedHashMap<String,Integer> winners =new  LinkedHashMap<String,Integer>();
+	public static long firstAnswer = 0;
+	public static int lastHalfMinChats = 0;
+	public ShadyBotty b;
+	public static TriviaThread tr;
+
+
+
+
+	public TriviaThread(ShadyBotty b) {
+		off = true;
+		this.b=b;
+		if (questions.size() == 0)
+			reload();
+		tr = this;
+	}
+
+	public static void reload() {
+		try {
+			InputStream is = TriviaThread.class.getResourceAsStream("HearthstoneQuestions.txt");
+			InputStreamReader isr = new InputStreamReader(is, Charset.forName("UTF-8"));
+			BufferedReader in = new BufferedReader(isr);
+			System.out.println("! no excep");
+			for (int i = 0; i < QUESTIONAMOUNT; i++) {
+				questions.add(in.readLine());
+			}
+
+			in.close();
+		} catch ( IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void run() {
+		Random rand = new Random();
+		while (!off) {
+			
+			if (questions.size() == 0)
+				reload();
+			int randomNum = rand.nextInt(questions.size());			
+			String nextQuestion = questions.get(randomNum);
+			questions.remove(randomNum);
+			System.out.println("!" + nextQuestion);
+			separateQuestion(nextQuestion); //separates the questions based on the ©
+			System.out.println(topic + "  " + question);
+			b.sendMessage(ShadyBottyMain.ROOM, "[" + randomNum + "] topic: " + topic + ". Question: " + question);
+			questionOn();// wait till the question is done, whether answered or ran out of time.
+			topic = "";
+			
+			if (hasWinner())  {
+				String toChatMsg = getWinnerStringAddPoints();
+				System.out.println(toChatMsg);
+				b.sendMessage(ShadyBottyMain.ROOM, toChatMsg);
+				winners.clear();
+				answers.clear();
+				firstAnswer = 0;
+			} else {
+				b.sendMessage(ShadyBottyMain.ROOM, "the answer was: " + displayAnswer + ". Nobody has guessed correctly! motionRekt");
+					
+				System.out.println("the answer was: " + displayAnswer + ". Nobody has guessed correctly! motionRekt");
+				answers.clear();
+			}
+			try {
+				Thread.sleep(180000);
+			} catch (InterruptedException e) {
+
+			}
+			
+		}
+	}
+
+	
+	public void questionOn() {
+		Long start = System.currentTimeMillis();
+		while (System.currentTimeMillis() - start <= 30000 && firstAnswer == 0)
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		if (hasWinner())
+			try {
+				Thread.sleep(2500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
+
+
+	public void separateQuestion(String qu) {
+		String[] sepQu = qu.split("©");
+		topic = sepQu[0];
+		question = sepQu[1];
+		hint = sepQu[2];
+		displayAnswer = sepQu[3];
+		System.out.println(displayAnswer);
+		for (int i = 3; i < sepQu.length; i++) {
+			answers.add(sepQu[i].replaceAll("[^a-zA-Z0-9]","").toLowerCase());
+			System.out.println(sepQu[i].replaceAll("[^a-zA-Z0-9]","").toLowerCase() );
+		}
+	}
+
+	public boolean hasWinner() {
+		if (winners.size() == 0)
+			return false;
+		return true;
+	}
+
+	public static void addWinner(String winner) {
+		if (firstAnswer == 0) {
+			winners.put(winner, 30);
+			firstAnswer = System.currentTimeMillis();
+			return;
+		}//check if question has already been answered, if not, input time of first answer and give guy points
+
+		Long now = System.currentTimeMillis();
+		if (!winners.containsKey(winner)) {
+			if (now - firstAnswer < 500)
+				winners.put(winner, 25);
+			else 	if (now - firstAnswer < 1100)
+				winners.put(winner, 20);
+			else 	if (now - firstAnswer < 1800)
+				winners.put(winner, 15);
+			else 	if (now - firstAnswer < 2700)
+				winners.put(winner, 10);
+			else if (now - firstAnswer < 3800)
+				winners.put(winner, 5);		
+		}
+	}
+
+
+
+
+	
+	public String getWinnerStringAddPoints() {
+		
+		String winner2 = winners.keySet().iterator().next();
+		 String toChat = "the answer was: " + displayAnswer + ". motionTens Winner(30p): " + Nicknames.getNick(winner2)+ ".";
+		 Points.addPoints(winner2, 30);
+		 winners.remove(winner2);
+		int last = 30;
+		if (winners.size() == 0)
+			return toChat;
+		for (String a : winners.keySet()) {
+			Points.addPoints(a, winners.get(a));
+			if (winners.get(a) != last) {
+				last = winners.get(a);
+				toChat = toChat.substring(0, toChat.length()-1) + ". " + last + "p:";
+			}
+			toChat += " " + Nicknames.getNick(a) + ",";
+		}
+		toChat = toChat.substring(0, toChat.length()-1) + ".";
+		return toChat;
+	}
+	
+	public static void main(String[] args) {
+		reload();
+		System.out.println(questions.get(QUESTIONAMOUNT-1));
+	}
+
+}
